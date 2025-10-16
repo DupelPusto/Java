25d5df8562559a6b2da8098bee33a443e23afc58
Боріс Євгеній Вадимович <borisjenya2@gmail.com>
2025-10-16 14:45:20 +0300

    Update PublicUtilities.java

--- OOP/OOPPractice 3/PublicUtilities.java
+++ OOP/OOPPractice 3/PublicUtilities.java
@@ -3,6 +3,7 @@ package com.OOP.clases;
 import java.time.LocalDateTime;
 
 public class PublicUtilities {
-    protected static double payment;
-    protected static LocalDateTime paymentDate;
+    static double payment;
+    static LocalDateTime paymentDate;
 }
+
